dd0900b5b04f1df6381f4ec095653944c7d26297
Sabirov Jakhongir <jahon.080879@gmail.com>
2020-09-30 18:05:44 +0500

    Learn TexnoStream

---
+++ app/src/main/java/com/example/mailru/HelloUserActivity.java
@@ -0,0 +1,15 @@
+package com.example.mailru;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class HelloUserActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_user);
+
+    }
+}
---
+++ app/src/main/java/com/example/mailru/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mailru;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_texnopolis);
+    }
+}
