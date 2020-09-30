42650888178724b1134751d6a6816553d9200372
Sabirov Jakhongir <jahon.080879@gmail.com>
2020-09-30 18:18:52 +0500

    add little bit code inside of it

--- app/src/main/java/com/example/mailru/HelloUserActivity.java
+++ app/src/main/java/com/example/mailru/HelloUserActivity.java
@@ -2,14 +2,28 @@ package com.example.mailru;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 public class HelloUserActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear_user);
+        EditText editText = findViewById(R.id.edit);
+        Button btn = findViewById(R.id.btn);
+        TextView textView = findViewById(R.id.output);
 
+        textView.setVisibility(View.INVISIBLE);
+
+        btn.setOnClickListener(e ->{
+            textView.setText(editText.getText());
+            textView.setVisibility(View.VISIBLE);
+        });
     }
 }
